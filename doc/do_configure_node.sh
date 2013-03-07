992a5870409fb796b07a8251b8494c100f1d99a6
Fabien Le Mentec <texane@gmail.com>
2013-03-07 15:56:30 -0600

    [ fix ] do_confgure_node.sh typo

--- doc/do_configure_node.sh
+++ doc/do_configure_node.sh
@@ -5,6 +5,6 @@ CROSS_COMPILE=/home/texane/repo/lfs/_work_rpib/host_install/armv6-rpi-linux-gnue
 export AR=$CROSS_COMPILE\ar
 export CC=$CROSS_COMPILE\gcc
 export CXX=$CROSS_COMPILE\g++
-export LINK=$CROSS_COMPIL\g++
+export LINK=$CROSS_COMPILE\g++
 
 ./configure --without-snapshot --dest-cpu=arm --dest-os=linux --prefix=`pwd`/arm_install_dir
